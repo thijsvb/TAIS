e7a907464d62576c7525faf908e85f2ade0d4f01
Thijs <thijsvb@gmail.com>
2021-05-18 15:38:14 +0200

    skimage image processing

--- fits_to_stl/main.py
+++ fits_to_stl/main.py
@@ -6,43 +6,57 @@ from astropy.io import fits
 #Plotting
 from matplotlib.pyplot import figure, show
 #image manipulation
-import cv2
+from skimage import data, io, exposure, filters, restoration, morphology, transform
 #custom code
 from data_to_stl import *
 
 # Open file
 f = fits.open("NGC_4594_SDSS_i bms2014.fits")
 data = f[0].data
-#prep then log data to make peaks less steep
-data -= data.min()
-data += 1
-# data = np.log10(np.log10(data)+1)
-data = np.log10(data)
 
+# #prep then log data to make peaks less steep
+# data -= data.min()
+# data += 1
+# # data = np.log10(np.log10(data)+1)
+# data = np.log10(data)
+
+# IMAGE PROCESSING
 # scale data resolution
 Nres = int(50//0.3) # rough estimation for needed array length based on nozzle size (0.3) and model size (50)
-data = cv2.resize(data, (Nres,Nres), interpolation=cv2.INTER_NEAREST)
-
-# cut of noise
-thold = 0.2
-data -= thold
-ncols, nrows = data.shape
-for i in range(ncols):
-    for j in range(nrows):
-        if data[i][j] < 0:
-            data[i][j] = 0
+scalef = data.shape[0]//Nres
+data = transform.downscale_local_mean(data, (scalef, scalef))
+# chop
+# mask = data > 25
+# data[mask] = 25
+#equalize histogram
+data = exposure.equalize_hist(data, nbins=256, mask="none")
+# gamma correction
+data = exposure.adjust_gamma(data, 2)
+# log correction
+data = exposure.adjust_log(data, 3)
+logcor = data.copy()
+# denoise
+# data = restoration.denoise_tv_chambolle(data, weight=0.05, multichannel=False)
+# tophat filter
+selem = morphology.disk(1)
+res = morphology.white_tophat(data, selem)
+data = data - res
 
 # plot data
 fig = figure()
-frame = fig.add_subplot(1, 2, 1)
+frame = fig.add_subplot(2, 2, 1)
 frame.imshow(data, cmap="Greys")
-frame = fig.add_subplot(1, 2, 2)
+frame = fig.add_subplot(2, 2, 2)
 frame.hist(data.flatten(), bins=100)
+frame = fig.add_subplot(2, 2, 3)
+frame.imshow(logcor, cmap="Greys")
+frame = fig.add_subplot(2, 2, 4)
+frame.hist(logcor.flatten(), bins=100)
 show()
 
 size = (50, 50, 11) #x,y,z in mm
 #Creating the list of faces out of the array
-data_to_stl("sombrero_downscaled_noisecutoff.stl",data,size)
+data_to_stl("sombrero_skimaged.stl",data,size)
 
 #TEST DATA
 # # Initializing value of x-axis and y-axis
